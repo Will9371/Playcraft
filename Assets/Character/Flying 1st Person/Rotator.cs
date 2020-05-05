fb329c4e0a7171047ad0b57b88b8e7a24bfbf318
WillPetillo <will.petillo@gmail.com>
2020-05-04 19:36:51 -0700

    Refactored 1st person fly prefab
    Seperated movement from rotation logic, generalized naming.

---
+++ Assets/Character/Flying 1st Person/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] bool invertX;
+    [SerializeField] bool invertY = true;
+    private float x, y;
+
+    public void Rotate(Vector2 value)
+    {
+        x += value.y * (invertY ? -1f : 1f);
+        y += value.x * (invertX ? -1f : 1f);
+        transform.eulerAngles = new Vector3(x, y, 0);
+    }
+}
