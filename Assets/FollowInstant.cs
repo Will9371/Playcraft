3dbffd863d05c2c9d5e4859d180d96ef1bc77f5a
WillPetillo <will.petillo@gmail.com>
2020-05-05 14:34:46 -0700

    Added crouch animations
    Added animations + logic for crouch idle and walk with variations for forward, left, and right directions.

--- Assets/FollowInstant.cs
+++ Assets/FollowInstant.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 
-public class Follow : MonoBehaviour
+public class FollowInstant : MonoBehaviour
 {
     public Transform target;
 
     void LateUpdate()
     {
+        if (!target) return;
         transform.position = target.position;
         transform.rotation = target.rotation;
     }
