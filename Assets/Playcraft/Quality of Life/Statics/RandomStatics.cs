e047fefb434746ebce22eec1603a5a9e246a4cd2
Will Petillo <will.petillo@gmail.com>
2021-10-02 01:29:46 -0700

    Added dodge movement to Sword Trainer.

--- Assets/Playcraft/Quality of Life/Statics/RandomStatics.cs
+++ Assets/Playcraft/Quality of Life/Statics/RandomStatics.cs
@@ -9,6 +9,8 @@ namespace Playcraft
     public static class RandomStatics
     {
         public static bool CoinToss(float center = 0.5f) { return Random.Range(0f, 1f) > center; }
+        
+        public static WaitForSeconds RandomWait(Vector2 range) { return new WaitForSeconds(Random.Range(range.x, range.y)); }
     
         public static int RandomNoRepeat(int min, int max, int prior)
         {
--- Assets/Playcraft/Quality of Life/Type Based/Int/GetRandomInt.cs
+++ Assets/Playcraft/Quality of Life/Type Based/Int/GetRandomInt.cs
@@ -4,15 +4,9 @@ namespace Playcraft
 {
     public class GetRandomInt : MonoBehaviour
     {
-        #pragma warning disable 0649
         [SerializeField] int minimum;
         [SerializeField] int maximum;
         [SerializeField] IntEvent Output;
-        #pragma warning restore 0649
-        
-        public void Input()
-        {
-            Output.Invoke(Random.Range(minimum, maximum + 1));
-        }
+        public void Input() { Output.Invoke(Random.Range(minimum, maximum + 1)); }
     }
 }
